2ef4ccaca2398446eec3b26ef8ce5f8299fe0184
pengyl <pengyl@kingnet.com>
2019-03-14 11:47:10 +0800

    登录页修改：input增加icon、接口处理错误

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
-import React, {PureComponent, Fragment} from 'react'
-import {Button, Row, Form, Input, Icon} from 'antd'
+import React, {Fragment, PureComponent} from 'react'
+import {Button, Form, Icon, Input, Row} from 'antd'
 import styles from './Login.module.less'
 import {setToken} from '../../utils/tools'
 import {FormattedMessage} from 'react-intl'
@@ -10,90 +10,96 @@ const FormItem = Form.Item
 @inject('rootStore')
 @Form.create()
 class Login extends PureComponent {
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const {form} = this.props
-    const {validateFieldsAndScroll} = form
-    validateFieldsAndScroll(async (errors, values) => {
-      if (errors) {
-        return
-      }
-      const result = await Api.login(values, {mock: true})
-      setToken(result.token)
-      this.props.history.replace('/app/dashboard')
-    })
-  }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const {form} = this.props
+        const {validateFieldsAndScroll} = form;
+        validateFieldsAndScroll(async (errors, values) => {
+            if (errors) {
+                return
+            }
+            await Api.login(values, {mock: true}).then(res => {
+                console.log("login req::", res);
+                if (res.code == 0) {
+                    setToken(res.token)
+                    this.props.history.replace('/app/dashboard')
+                }
+            });
+        })
+    }
 
-  render() {
-    const {form} = this.props
-    const {getFieldDecorator} = form
-    return (
-      <Fragment>
-        <div className={styles.form}>
-          <div className={styles.logo}>
-            {/*<img alt='logo' src={config.logoPath}/>*/}
-            <span>{Config.siteName}</span>
-          </div>
-          <Form onSubmit={this.handleSubmit}>
-            <FormItem hasFeedback>
-              {getFieldDecorator('username', {
-                rules: [{required: true}]
-              })(
-                <Input
-                  placeholder={`Username`}
-                />
-              )}
-            </FormItem>
-            <FormItem hasFeedback>
-              {getFieldDecorator('password', {
-                rules: [{required: true}]
-              })(
-                <Input
-                  type='password'
-                  placeholder={`Password`}
-                />
-              )}
-            </FormItem>
-            <Row>
-              <Button
-                type='primary'
-                htmlType={'submit'}>
-                <FormattedMessage id='intl.signIn'/>
-              </Button>
-              <p>
-                <span>Username：guest</span>
-                <span>Password：guest</span>
-              </p>
-            </Row>
-          </Form>
-        </div>
-        <div className={styles.footer}>
-          <footer className={styles['footer-view']}>
-            <div className={styles['footer-view-links']}>
-              <span>
-                <a
-                  title='github'
-                  rel="noopener noreferrer"
-                  target='_blank'
-                  href='https://github.com/shx996/react-antd-admin'>
-                <Icon type="github"/>
-              </a>
-              </span>
-              <span
-                onClick={() => {
-                  this.props.rootStore.changeLocale('en')
-                }}>English</span>
-              <span
-                onClick={() => {
-                  this.props.rootStore.changeLocale('zh')
-                }}>中文</span>
-            </div>
-            <div className={styles['footer-copyright']}>Ant Design Admin © 2019 shx996</div>
-          </footer>
-        </div>
-      </Fragment>
-    )
-  }
+    render() {
+        const {form} = this.props
+        const {getFieldDecorator} = form
+        return (
+            <Fragment>
+                <div className={styles.form}>
+                    <div className={styles.logo}>
+                        {/*<img alt='logo' src={config.logoPath}/>*/}
+                        <span>{Config.siteName}</span>
+                    </div>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormItem hasFeedback>
+                            {getFieldDecorator('username', {
+                                rules: [{required: true, message: '请输入用户名!'}]
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder="请输入用户名"
+                                />
+                            )}
+                        </FormItem>
+                        <FormItem hasFeedback>
+                            {getFieldDecorator('password', {
+                                rules: [{required: true, message: '请输入密码!'}]
+                            })(
+                                <Input
+                                    type='password'
+                                    prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                    placeholder={`请输入密码`}
+                                />
+                            )}
+                        </FormItem>
+                        <Row>
+                            <Button
+                                type='primary'
+                                htmlType={'submit'}>
+                                <FormattedMessage id='intl.signIn'/>
+                            </Button>
+                            <p>
+                                <span>Username：admin</span>
+                                <span>Password：admin</span>
+                            </p>
+                        </Row>
+                    </Form>
+                </div>
+                <div className={styles.footer}>
+                    <footer className={styles['footer-view']}>
+                        <div className={styles['footer-view-links']}>
+                          <span>
+                            <a
+                                title='github'
+                                rel="noopener noreferrer"
+                                target='_blank'
+                                href='https://github.com/shx996/react-antd-admin'>
+                            <Icon type="github"/>
+                          </a>
+                          </span>
+                            <span
+                                onClick={() => {
+                                    this.props.rootStore.changeLocale('en')
+                                }}>English</span>
+                            <span
+                                onClick={() => {
+                                    this.props.rootStore.changeLocale('zh')
+                                }}>中文</span>
+                        </div>
+                        <div className={styles['footer-copyright']}>Ant Design Admin © 2019 perany</div>
+                    </footer>
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default Login
