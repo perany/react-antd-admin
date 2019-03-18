74af9e3ac33613fda2c783a0f9a866c0ff17ad44
pengyl <pengyl@kingnet.com>
2019-03-18 10:43:51 +0800

    菜单栏

--- src/components/Layout/SiderCustom.js
+++ src/components/Layout/SiderCustom.js
@@ -1,116 +1,130 @@
 import React, {Component, Fragment} from 'react'
 import {Link, withRouter} from 'react-router-dom'
-import {Icon, Menu, Layout} from 'antd'
+import {Icon, Layout, Menu} from 'antd'
 import PropTypes from 'prop-types'
 import routes from '../../routes/config'
 import {inject} from 'mobx-react'
+import styles from './SiderCustom.module.less'
 
+@inject('rootStore')
 class SiderCustom extends Component {
-  static propTypes = {
-    collapsed: PropTypes.bool
-  }
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.collapsed !== state.collapsed || props.location.pathname !== SiderCustom.pathname) {
-      SiderCustom.pathname = props.location.pathname
-      return {
-        collapsed: props.collapsed,
-        ...SiderCustom.setMenuOpen(props),
-      }
+    //propType类型检查器
+    static propTypes = {
+        collapsed: PropTypes.bool
     }
-    return null
-  }
 
-  static setMenuOpen = (props) => {
-    const {pathname} = props.location
-    return {
-      openKey: props.collapsed ? [] : SiderCustom.openKey,
-      selectedKey: pathname
+    static getDerivedStateFromProps(props, state) {
+        console.log('******getDerivedStateFromProps', props, state)
+        if (props.collapsed !== state.collapsed || props.location.pathname !== SiderCustom.pathname) {
+            SiderCustom.pathname = props.location.pathname
+            return {
+                collapsed: props.collapsed,
+                ...SiderCustom.setMenuOpen(props),
+            }
+        }
+        return null
     }
-  }
 
-  static openKey = []
-  static pathname = ''
+    static setMenuOpen = (props) => {
+        const {pathname} = props.location
+        return {
+            openKey: props.collapsed ? [] : SiderCustom.openKey,
+            selectedKey: pathname
+        }
+    }
 
-  state = {
-    openKey: [],
-    selectedKey: '',
-    collapsed: false
-  }
+    static openKey = []
+    static pathname = ''
 
-  componentDidMount() {
-    SiderCustom.openKey = [this.props.location.parentKey] || []
-    SiderCustom.pathname = this.props.location.pathname
-    const state = SiderCustom.setMenuOpen(this.props)
-    this.setState(state)
-  }
+    state = {
+        openKey: [],
+        selectedKey: '',
+        collapsed: true
+    }
 
-  menuClick = e => {
-    this.setState({
-      selectedKey: e.key
-    })
-  }
+    componentDidMount() {
+        console.log('******componentDidMount', this.props, this.state)
+        SiderCustom.openKey = [this.props.location.parentKey] || []
+        SiderCustom.pathname = this.props.location.pathname
+        const state = SiderCustom.setMenuOpen(this.props)
+        this.setState(state)
+    }
 
-  openMenu = v => {
-    SiderCustom.openKey = v
-    this.setState({
-      openKey: v
-    })
-  }
+    menuClick = e => {
+        console.log(e, this.state, this.props)
+        this.setState({
+            selectedKey: e.key
+        })
+    }
 
-  renderMenuItem = item => {
-    const {rootStore} = this.props
-    const title = rootStore.locale !== 'en' ? item[rootStore.locale + 'Title'] || item.title : item.title
-    return (
-      <Menu.Item
-        key={item.key}
-      >
-        <Link to={(item.path || item.key) + (item.query || '')}>
-          {item.icon && <Icon type={item.icon}/>}
-          <span className='nav-item'>{title}</span>
-        </Link>
-      </Menu.Item>
-    )
-  }
+    openMenu = v => {
+        SiderCustom.openKey = v
+        this.setState({
+            openKey: v
+        })
+    }
 
-  renderSubMenu = item => {
-    const {rootStore} = this.props
-    const title = rootStore.locale !== 'en' ? item[rootStore.locale + 'Title'] || item.title : item.title
-    return (
-      <Menu.SubMenu
-        key={item.key}
-        title={
-          <Fragment>
-            {item.icon && <Icon type={item.icon}/>}
-            <span className='nav-item'>{title}</span>
-          </Fragment>
-        }>
-        {item.subs.map(item => this.renderMenuItem(item))}
-      </Menu.SubMenu>
-    )
-  }
+    renderMenuItem = item => {
+        const {rootStore} = this.props
+        const title = rootStore.locale !== 'en' ? item[rootStore.locale + 'Title'] || item.title : item.title
+        return (
+            <Menu.Item
+                key={item.key}
+            >
+                <Link to={(item.path || item.key) + (item.query || '')}>
+                    {item.icon && <Icon type={item.icon}/>}
+                    <span className='nav-item'>{title}</span>
+                </Link>
+            </Menu.Item>
+        )
+    }
 
-  render() {
-    return (
-      <Layout.Sider
-        width={240}
-        trigger={null}
-        collapsible
-        collapsed={this.props.collapsed}
-      >
-        <div className='logo'/>
-        <Menu theme='dark'
-              mode='inline'
-              inlineCollapsed={this.props.collapsed}
-              selectedKeys={[this.state.selectedKey]}
-              openKeys={this.state.openKey}
-              onClick={this.menuClick}
-              onOpenChange={this.openMenu}>
-          {routes.map(r => r.component ? this.renderMenuItem(r) : this.renderSubMenu(r))}
-        </Menu>
-      </Layout.Sider>
-    )
-  }
+    renderSubMenu = item => {
+        const {rootStore} = this.props
+        const title = rootStore.locale !== 'en' ? item[rootStore.locale + 'Title'] || item.title : item.title
+        return (
+            <Menu.SubMenu
+                key={item.key}
+                title={
+                    <>
+                        {item.icon && <Icon type={item.icon}/>}
+                        <span className='nav-item'>{title}</span>
+                    </>
+                }>
+                {item.subs.map(item => this.renderMenuItem(item))}
+            </Menu.SubMenu>
+        )
+    }
+
+    renderTrigger = (name) => {
+        return (
+            <div className={styles.trigger}>{name}</div>
+        )
+    }
+
+    render() {
+        return (
+            <Layout.Sider
+                width={240}
+                collapsedWidth={90}
+                trigger={this.renderTrigger('KINGNET')}
+                collapsible
+                collapsed={this.props.collapsed}
+            >
+                <div className={styles.logo}>PERANY</div>
+                <Menu theme='dark'
+                      mode='inline'
+                      inlineCollapsed={this.props.collapsed}
+                      selectedKeys={[this.state.selectedKey]}
+                      openKeys={this.state.openKey}
+                      onClick={this.menuClick}
+                      onOpenChange={this.openMenu}>
+                    {routes.map(r => r.component ? this.renderMenuItem(r) : this.renderSubMenu(r))}
+                </Menu>
+            </Layout.Sider>
+        )
+    }
 }
 
-export default withRouter(inject('rootStore')(SiderCustom))
+export default withRouter(SiderCustom)
